58c56ac5bfb64f4414c1ea9b4b96d600a82e20b5
lacey <hiu@example.com>
2019-01-09 14:25:53 +0000

    pass test 3

--- models/cinema.js
+++ models/cinema.js
@@ -15,3 +15,8 @@ Cinema.prototype.findByTitle = function (title) {
   const filmTitle = this.films.filter(film => film.title === title);
   return filmTitle[0];
 };
+
+Cinema.prototype.findByGenre = function (genre) {
+  const filmGenre = this.films.filter(film => film.genre === genre);
+  return filmGenre
+};
